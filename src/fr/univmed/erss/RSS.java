1678075864e701005b57664d544036c99839676b
Pierre-Louis Conte <Plouis.conte@gmail.com>
2011-05-11 18:04:50 +0200

    Modification de l'objet RSS et ajout d'un FluxActivity (TODO)

--- src/fr/univmed/erss/RSS.java
+++ src/fr/univmed/erss/RSS.java
@@ -1,6 +1,27 @@
 package fr.univmed.erss;
 
 public class RSS {
-	public static final String URL_HOTEL = "http://romain.de-luca.perso.esil.univmed.fr/promotions_hotels.xml";
-	public static final String URL_AGENDA = "http://romain.de-luca.perso.esil.univmed.fr/Agenda_Culturel_Marseille.xml";
+	public String url;
+//= "http://romain.de-luca.perso.esil.univmed.fr/promotions_hotels.xml";
+//= "http://romain.de-luca.perso.esil.univmed.fr/Agenda_Culturel_Marseille.xml";
+	public boolean checked;
+	
+	public RSS(String url, boolean checked) {
+		super();
+		this.url = url;
+		this.checked = checked;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public boolean isChecked() {
+		return checked;
+	}
+	public void setChecked(boolean checked) {
+		this.checked = checked;
+	}
+	
 }
